8d6da2f6b564965731fd99a03375dab6aa83ed2d
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2020-02-13 14:07:22 +0200

    Adding less configurations

---
+++ EPlast/EPlast/gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    less = require("gulp-less"); // adding less module
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+// registrating task for transforming styles.less into css file 
+gulp.task("less", function () {
+    return gulp.src('./wwwroot/less/styles.less')
+        .pipe(less())
+        .pipe(gulp.dest(paths.webroot + '/css' ))
+});
