c616f533dd14256014dc20a03425d4bfed056339
Roman <brych.126@gmail.com>
2020-02-18 16:04:58 +0200

    RB: AddEventMigration

---
+++ EPlast/EPlast.DataAccess/Migrations/20200218140008_Adding-Event-Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class AddingEventMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventName = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    EventDateStart = table.Column<DateTime>(nullable: false),
+                    EventDateEnd = table.Column<DateTime>(nullable: false),
+                    Eventlocation = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
