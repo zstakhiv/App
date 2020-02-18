41087f62d551e2024e0cf7f4670387abe5817c53
Anna <hileta.anna@gmail.com>
2020-02-18 21:28:48 +0200

    HA-Added Repositories, also added Entities to DB Context

--- EPlast/EPlast.DataAccess/Repositories/Contracts/IRepositoryWrapper.cs
+++ EPlast/EPlast.DataAccess/Repositories/Contracts/IRepositoryWrapper.cs
@@ -1,9 +1,16 @@
-﻿namespace EPlast.DataAccess.Repositories
+﻿using EPlast.DataAccess.Repositories.Contracts;
+
+namespace EPlast.DataAccess.Repositories
 {
     public interface IRepositoryWrapper
     {
         IUserRepository User { get; }
         INationalityRepository Nationality { get; }
+        ICityAdministrationRepository CityAdministration{ get; }
+        ICityDocumentsRepository CityDocuments { get; }
+        ICityDocumentTypeRepository CityDocumentType { get; }
+        ICityMembersRepository CityMembers { get; }
+        ICityRepository City { get; }
         void Save();
     }
 }
--- EPlast/EPlast.DataAccess/Repositories/RepositoryWrapper.cs
+++ EPlast/EPlast.DataAccess/Repositories/RepositoryWrapper.cs
@@ -1,10 +1,17 @@
-﻿namespace EPlast.DataAccess.Repositories
+﻿using EPlast.DataAccess.Repositories.Contracts;
+
+namespace EPlast.DataAccess.Repositories
 {
     public class RepositoryWrapper : IRepositoryWrapper
     {
         private EPlastDBContext _dbContext;
         private IUserRepository _user;
         private INationalityRepository _nationality;
+        private ICityAdministrationRepository _cityAdministration;
+        private ICityDocumentsRepository _cityDocuments;
+        private ICityDocumentTypeRepository _cityDocumentType;
+        private ICityMembersRepository _cityMembers;
+        private ICityRepository _city;
 
         public IUserRepository User
         {
@@ -29,6 +36,71 @@
                 }
 
                 return _nationality;
+            }
+        }
+
+        public ICityAdministrationRepository CityAdministration
+        {
+            get
+            {
+                if (_cityAdministration == null)
+                {
+                    _cityAdministration = new CityAdministrationRepository(_dbContext);
+                }
+
+                return _cityAdministration;
+            }
+        }
+
+        public ICityDocumentsRepository CityDocuments
+        {
+            get
+            {
+                if (_cityDocuments == null)
+                {
+                    _cityDocuments = new CityDocumentsRepository(_dbContext);
+                }
+
+                return _cityDocuments;
+            }
+        }
+
+        public ICityDocumentTypeRepository CityDocumentType
+        {
+            get
+            {
+                if (_cityDocumentType == null)
+                {
+                    _cityDocumentType = new CityDocumentTypeRepository(_dbContext);
+                }
+
+                return _cityDocumentType;
+            }
+        }
+
+        public ICityMembersRepository CityMembers
+        {
+            get
+            {
+                if (_cityMembers == null)
+                {
+                    _cityMembers = new CityMembersRepository(_dbContext);
+                }
+
+                return _cityMembers;
+            }
+        }
+
+        public ICityRepository City
+        {
+            get
+            {
+                if (_city == null)
+                {
+                    _city = new CityRepository(_dbContext);
+                }
+
+                return _city;
             }
         }
 
