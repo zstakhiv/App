ca535f355f5df323f8e7efaadab9e3982545dc05
ABashko <bashko.zllv@gmail.com>
2020-02-20 22:15:58 +0200

    Delete unnecessary fields from AnnualReports

---
+++ EPlast/EPlast.DataAccess/Migrations/20200220200722_Delete_unnecessary_fields_from_AnnualReports.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPlast.DataAccess.Migrations
+{
+    public partial class Delete_unnecessary_fields_from_AnnualReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BeneficiaryCount",
+                table: "AnnualReports");
+
+            migrationBuilder.DropColumn(
+                name: "BirdieSocketCount",
+                table: "AnnualReports");
+
+            migrationBuilder.DropColumn(
+                name: "NotNameCount",
+                table: "AnnualReports");
+
+            migrationBuilder.DropColumn(
+                name: "SocketsCount",
+                table: "AnnualReports");
+
+            migrationBuilder.DropColumn(
+                name: "SwarmsSavedCount",
+                table: "AnnualReports");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BeneficiaryCount",
+                table: "AnnualReports",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BirdieSocketCount",
+                table: "AnnualReports",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NotNameCount",
+                table: "AnnualReports",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SocketsCount",
+                table: "AnnualReports",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SwarmsSavedCount",
+                table: "AnnualReports",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
