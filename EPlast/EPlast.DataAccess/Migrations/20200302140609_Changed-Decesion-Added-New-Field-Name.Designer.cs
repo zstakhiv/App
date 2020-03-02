9fda81051f64b9a1837d2304c754ff547422eeaa
Michael Kunynets <xfreed1@gmail.com>
2020-03-02 16:08:41 +0200

    Add new migration

--- EPlast/EPlast.DataAccess/Migrations/20200302140609_Changed-Decesion-Added-New-Field-Name.Designer.cs
+++ EPlast/EPlast.DataAccess/Migrations/20200302140609_Changed-Decesion-Added-New-Field-Name.Designer.cs
@@ -4,14 +4,16 @@ using EPlast.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EPlast.DataAccess.Migrations
 {
     [DbContext(typeof(EPlastDBContext))]
-    partial class EPlastDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200302140609_Changed-Decesion-Added-New-Field-Name")]
+    partial class ChangedDecesionAddedNewFieldName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
