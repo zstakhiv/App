e2e79a6a213a341d96afe5205ef8701b7fa796a9
Roman <brych.126@gmail.com>
2020-03-07 22:06:44 +0200

    BR: split error handling

--- EPlast/EPlast/Controllers/HomeController.cs
+++ EPlast/EPlast/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace EPlast.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+             return View();
         }
 
         public IActionResult Privacy()
