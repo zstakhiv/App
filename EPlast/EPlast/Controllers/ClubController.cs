02caa6dc50a3c50a05dab082d0f9fdf4c9b4eff4
Anna <hileta.anna@gmail.com>
2020-03-09 23:10:26 +0200

    HA: Added ViewModel, Controller, Index.cshtml and migration to add logo to the club

---
+++ EPlast/EPlast/Controllers/ClubController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPlast.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPlast.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly DataAccess.Repositories.IRepositoryWrapper _repoWrapper;
+
+        public ClubController(DataAccess.Repositories.IRepositoryWrapper repoWrapper)
+        {
+            _repoWrapper = repoWrapper;
+        }
+        public IActionResult Index()
+        {
+            List<ClubViewModel> clubs = new List<ClubViewModel>(
+                _repoWrapper.Club
+                .FindAll()
+                .Take(200)
+                .Select(club => new ClubViewModel { Club = club })
+                .ToList());
+
+            return View(clubs);
+        }
+    }
+}
