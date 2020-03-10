d3cf268351a8664b30bf2d5b245e376a43e925f3
Roman <brych.126@gmail.com>
2020-03-10 13:01:11 +0200

    BR:Add view models for actions

---
+++ EPlast/EPlast/ViewModels/Events/EventCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.ViewModels.Events
+{
+    public class EventCategoryViewModel
+    {
+        public int  ID { get; set; }
+        public string EventCategory { get; set; }
+    }
+}
