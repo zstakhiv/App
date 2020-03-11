07011605d3827add0a547e98c970c51063e1eb77
VolodymyrVerm <volodymyr33929@gmail.com>
2020-03-11 19:52:03 +0200

    Global change Edit page

--- EPlast/EPlast.DataAccess/Entities/Degree.cs
+++ EPlast/EPlast.DataAccess/Entities/Degree.cs
@@ -7,7 +7,8 @@ namespace EPlast.DataAccess.Entities
     {
         public int ID { get; set; }
 
-        [MaxLength(50, ErrorMessage = "DegreeName cannot exceed 50 characters")]
+        [Display(Name = "Ступінь")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Ступінь повинна складати від 3 до 20 символів")]
         public string Name { get; set; }
         public ICollection<Education> Educations { get; set; }
     }
