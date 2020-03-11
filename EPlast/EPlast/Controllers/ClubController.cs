5e5ecd2672e4f4e536ee937a7ee9b0210f17f4af
Anna <hileta.anna@gmail.com>
2020-03-11 23:16:43 +0200

    HA: Connecting to database (not too successfull)

--- EPlast/EPlast/Controllers/ClubController.cs
+++ EPlast/EPlast/Controllers/ClubController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EPlast.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 namespace EPlast.Controllers
 {
     public class ClubController : Controller
@@ -27,7 +27,19 @@ namespace EPlast.Controllers
         }
         public IActionResult Club(int index)
         {
-            return View(index);
+
+            var club = _repoWrapper.Club
+                .FindByCondition(q => q.ID == index)
+                .Include(c => c.ClubAdministration)
+                .ThenInclude(t => t.AdminType)
+                .Include(n => n.ClubAdministration)
+                .ThenInclude(t => t.ClubMembers)
+                .ThenInclude(us => us.User)
+                .Include(m => m.ClubMembers)
+                .ThenInclude(u => u.User)
+                .FirstOrDefault();
+              
+            return View(new ClubViewModel { Club = club});
         }
     }
 }
