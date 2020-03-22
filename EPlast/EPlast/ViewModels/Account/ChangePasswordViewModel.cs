e97989357477a00d5d38b000b86020333ec462ae
Андрій Шайнога <andrii.shainoha.pz.2018@lpnu.ua>
2020-03-22 19:46:02 +0200

    Added ChangePassword View

--- EPlast/EPlast/ViewModels/Account/ChangePasswordViewModel.cs
+++ EPlast/EPlast/ViewModels/Account/ChangePasswordViewModel.cs
@@ -10,8 +10,18 @@ namespace EPlast.ViewModels.Account
     {
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
         public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
         public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new Password")]
+        [Compare("NewPassword", ErrorMessage = 
+            "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
