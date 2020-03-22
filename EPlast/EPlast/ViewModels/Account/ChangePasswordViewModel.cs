50e4a5259592c2a29836e6ec865f5a2332f19466
Андрій Шайнога <andrii.shainoha.pz.2018@lpnu.ua>
2020-03-22 19:42:05 +0200

    Added class Changepasswordviewmodel

---
+++ EPlast/EPlast/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlast.ViewModels.Account
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
