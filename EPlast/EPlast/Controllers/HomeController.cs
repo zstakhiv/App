7881f611f71134d28d2d7844dd31531efbd47881
Андрій Шайнога <andrii.shainoha.pz.2018@lpnu.ua>
2020-03-24 22:22:57 +0200

    Fixed problems in HomeController

--- EPlast/EPlast/Controllers/HomeController.cs
+++ EPlast/EPlast/Controllers/HomeController.cs
@@ -1,19 +1,20 @@
 ﻿using EPlast.BussinessLayer.Interfaces;
 using EPlast.Models;
+using EPlast.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace EPlast.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger _logger;
         private readonly IEmailConfirmation _emailConfirmation;
 
-        public HomeController(ILogger logger, IEmailConfirmation emailConfirmation)
+        public HomeController(IEmailConfirmation emailConfirmation)
         {
-            _logger = logger;
             _emailConfirmation = emailConfirmation;
         }
 
@@ -55,7 +56,83 @@ namespace EPlast.Controllers
             return View("Views/Account/LoginAndRegister.cshtml");
         }
 
-         
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SendFeedBack(FeedBackViewModel feedBackViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Дані введені неправильно");
+                return View("Contacts");
+            }
+
+            await _emailConfirmation.SendEmailAsync("eplastdmnstrtr@gmail.com", 
+                "Питання користувачів",
+                "Питання",
+                feedBackViewModel.Email);
+
+            return View("Contacts");
+        }
+
+
+
+
+
+        /*[HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Дані введені неправильно");
+                return View("Register");
+            }
+
+            var registeredUser = await _userManager.FindByEmailAsync(registerVM.Email);
+            if (registeredUser != null)
+            {
+                ModelState.AddModelError("", "Користувач з введеною електронною поштою вже зареєстрований в системі");
+                return View("Register");
+            }
+            else
+            {
+                var user = new User()
+                {
+                    Email = registerVM.Email,
+                    UserName = registerVM.Email,
+                    LastName = registerVM.SurName,
+                    FirstName = registerVM.Name,
+                    ImagePath = "default.png",
+                    UserProfile = new UserProfile()
+                };
+
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+                await _userManager.AddToRoleAsync(user, "Користувач");
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", "Пароль має містити щонайменше 8 символів, цифри та літери");
+                    return View("Register");
+                }
+                else
+                {
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var confirmationLink = Url.Action(
+                        nameof(ConfirmingEmail),
+                        "Account",
+                        new { code = code, userId = user.Id },
+                        protocol: HttpContext.Request.Scheme);
+
+                    await _emailConfirmation.SendEmailAsync(registerVM.Email, "Підтвердження реєстрації ",
+                        $"Підтвердіть реєстрацію, перейшовши за :  <a href='{confirmationLink}'>посиланням</a> ", "Адміністрація сайту EPlast");
+
+                    return View("AcceptingEmail");
+                }
+            }
+        }
+*/
+
 
     }
 }
