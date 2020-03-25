6599688f25736209f839de2f38f89c7c669e8790
Anna <hileta.anna@gmail.com>
2020-03-25 22:32:16 +0200

    HA: Fixed Controller and renewed pathes to images

--- EPlast/EPlast/Controllers/ClubController.cs
+++ EPlast/EPlast/Controllers/ClubController.cs
@@ -1,8 +1,12 @@
 ﻿using EPlast.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 
 namespace EPlast.Controllers
@@ -10,10 +14,11 @@ namespace EPlast.Controllers
     public class ClubController : Controller
     {
         private readonly DataAccess.Repositories.IRepositoryWrapper _repoWrapper;
-
-        public ClubController(DataAccess.Repositories.IRepositoryWrapper repoWrapper)
+        private readonly IHostingEnvironment _env;
+        public ClubController(DataAccess.Repositories.IRepositoryWrapper repoWrapper, IHostingEnvironment env)
         {
             _repoWrapper = repoWrapper;
+            _env = env;
         }
 
         public IActionResult Index()
@@ -29,7 +34,9 @@ namespace EPlast.Controllers
 
         public IActionResult Club(int index)
         {
-            var club = _repoWrapper.Club
+            try
+            {
+                var club = _repoWrapper.Club
                 .FindByCondition(q => q.ID == index)
                 .Include(c => c.ClubAdministration)
                 .ThenInclude(t => t.AdminType)
@@ -40,11 +47,18 @@ namespace EPlast.Controllers
                 .ThenInclude(u => u.User)
                 .FirstOrDefault();
 
-            return View(new ClubViewModel { Club = club });
+                return View(new ClubViewModel { Club = club });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
         public IActionResult ClubAdmins(int index)
         {
-            var club = _repoWrapper.Club
+            try
+            {
+                var club = _repoWrapper.Club
                 .FindByCondition(q => q.ID == index)
                 .Include(c => c.ClubAdministration)
                 .ThenInclude(t => t.AdminType)
@@ -53,35 +67,61 @@ namespace EPlast.Controllers
                 .ThenInclude(us => us.User)
                 .FirstOrDefault();
 
-            return View(new ClubViewModel { Club = club });
+                return View(new ClubViewModel { Club = club });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
         public IActionResult ClubMembers(int index)
         {
-            var club = _repoWrapper.Club
+            try
+            {
+                var club = _repoWrapper.Club
                 .FindByCondition(q => q.ID == index)
                 .Include(m => m.ClubMembers)
                 .ThenInclude(u => u.User)
                 .FirstOrDefault();
 
-            return View(new ClubViewModel { Club = club });
+                return View(new ClubViewModel { Club = club });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
         public IActionResult ClubFollowers(int index)
         {
-            var club = _repoWrapper.Club
+            try
+            {
+                var club = _repoWrapper.Club
                 .FindByCondition(q => q.ID == index)
                 .Include(m => m.ClubMembers)
                 .ThenInclude(u => u.User)
                 .FirstOrDefault();
 
-            return View(new ClubViewModel { Club = club });
+                return View(new ClubViewModel { Club = club });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
         public IActionResult ClubDescription(int index)
         {
-            var club = _repoWrapper.Club
+            try
+            {
+                var club = _repoWrapper.Club
                 .FindByCondition(q => q.ID == index)
                 .FirstOrDefault();
 
-            return View(new ClubViewModel { Club = club });
+                return View(new ClubViewModel { Club = club });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
 
         [HttpGet]
@@ -100,116 +140,46 @@ namespace EPlast.Controllers
                 return RedirectToAction("HandleError", "Error", new { code = 505 });
             }
         }
-        /*
+
         [HttpPost]
-        public IActionResult Edit(UserViewModel model, IFormFile file)
+        public IActionResult EditClub(ClubViewModel model, IFormFile file)
         {
-            try
+            try 
             {
-                var oldImageName = _repoWrapper.User.FindByCondition(i => i.Id == model.User.Id).FirstOrDefault().ImagePath;
+                var oldImageName = _repoWrapper.Club.FindByCondition(i => i.ID == model.Club.ID).FirstOrDefault().Logo;
                 if (file != null && file.Length > 0)
                 {
                     var img = Image.FromStream(file.OpenReadStream());
-                    var uploads = Path.Combine(_env.WebRootPath, "images\\Users");
-                    if (!string.IsNullOrEmpty(oldImageName) && !string.Equals(oldImageName, "default.png"))
+                    var uploads = Path.Combine(_env.WebRootPath, "images\\Club");
+                    if (!string.IsNullOrEmpty(oldImageName))
                     {
                         var oldPath = Path.Combine(uploads, oldImageName);
                         if (System.IO.File.Exists(oldPath))
                         {
                             System.IO.File.Delete(oldPath);
                         }
+
                     }
 
                     var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
                     var filePath = Path.Combine(uploads, fileName);
                     img.Save(filePath);
-                    model.User.ImagePath = fileName;
+                    model.Club.Logo = fileName;
                 }
                 else
                 {
-                    model.User.ImagePath = oldImageName;
+                    model.Club.Logo = oldImageName;
                 }
-
-                if (model.User.UserProfile.Nationality.ID == 0)
-                {
-                    string name = model.User.UserProfile.Nationality.Name;
-                    if (string.IsNullOrEmpty(name))
-                    {
-                        model.User.UserProfile.Nationality = null;
-                    }
-                    else
-                    {
-                        model.User.UserProfile.Nationality = new Nationality() { Name = name };
-                    }
-                }
-
-                if (model.User.UserProfile.Religion.ID == 0)
-                {
-                    string name = model.User.UserProfile.Religion.Name;
-                    if (string.IsNullOrEmpty(name))
-                    {
-                        model.User.UserProfile.Religion = null;
-                    }
-                    else
-                    {
-                        model.User.UserProfile.Religion = new Religion() { Name = name };
-                    }
-                }
-
-                Degree degree = model.User.UserProfile.Education.Degree;
-                if (model.User.UserProfile.Education.Degree.ID == 0)
-                {
-                    string name = model.User.UserProfile.Education.Degree.Name;
-                    if (string.IsNullOrEmpty(name))
-                    {
-                        model.User.UserProfile.Education.Degree = null;
-                    }
-                    else
-                    {
-                        model.User.UserProfile.Education.Degree = new Degree() { Name = name };
-                    }
-                }
-
-                if (model.User.UserProfile.Education.ID == 0)
-                {
-                    string placeOfStudy = model.User.UserProfile.Education.PlaceOfStudy;
-                    string speciality = model.User.UserProfile.Education.Speciality;
-                    if (string.IsNullOrEmpty(placeOfStudy) || string.IsNullOrEmpty(speciality))
-                    {
-                        model.User.UserProfile.Education = null;
-                    }
-                    else
-                    {
-                        model.User.UserProfile.Education = new Education() { PlaceOfStudy = placeOfStudy, Speciality = speciality, Degree = degree };
-                    }
-                }
-
-                if (model.User.UserProfile.Work.ID == 0)
-                {
-                    string placeOfWork = model.User.UserProfile.Work.PlaceOfwork;
-                    string position = model.User.UserProfile.Work.Position;
-                    if (string.IsNullOrEmpty(placeOfWork) || string.IsNullOrEmpty(position))
-                    {
-                        model.User.UserProfile.Work = null;
-                    }
-                    else
-                    {
-                        model.User.UserProfile.Work = new Work() { PlaceOfwork = placeOfWork, Position = position };
-                    }
-                }
-
-                _repoWrapper.UserProfile.Update(model.User.UserProfile);
-                _repoWrapper.User.Update(model.User);
+                _repoWrapper.Club.Update(model.Club);
                 _repoWrapper.Save();
-                _logger.LogInformation("User {0} {1} was edited profile and saved in the database", model.User.FirstName, model.User.LastName);
-                return RedirectToAction("UserProfile");
+                return RedirectToAction("Club", new { index = model.Club.ID});
             }
             catch (Exception e)
             {
-                _logger.LogError("Exception: {0}", e.Message);
+               
                 return RedirectToAction("HandleError", "Error", new { code = 505 });
             }
         }
-        */
+
     }
 }
