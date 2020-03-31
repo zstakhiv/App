0ceaae275d5fb68584fd1a5225e54643d290a0e5
Ilona Hirnyk (Maksymovych) <cat.in.tardis@gmail.com>
2020-03-31 21:15:44 +0300

    change citycontroller add cityViewModel & cityProfile

--- EPlast/EPlast/Controllers/CItyController.cs
+++ EPlast/EPlast/Controllers/CItyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EPlast.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EPlast.Controllers
@@ -9,6 +10,11 @@ namespace EPlast.Controllers
     public class CItyController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityProfile()
         {
             return View();
         }
