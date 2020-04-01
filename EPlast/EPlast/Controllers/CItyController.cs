25b129fd6e743a9001d604aff5a0e9f50086782d
Ilona Hirnyk (Maksymovych) <cat.in.tardis@gmail.com>
2020-04-01 12:02:41 +0300

    stash

--- EPlast/EPlast/Controllers/CItyController.cs
+++ EPlast/EPlast/Controllers/CItyController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using EPlast.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EPlast.DataAccess.Entities;
+using EPlast.ViewModels;
+
 
 namespace EPlast.Controllers
 {
@@ -20,9 +24,35 @@ namespace EPlast.Controllers
             return View();
         }
 
-        public IActionResult CityProfile()
+        public IActionResult CityProfile(int cityId)
         {
-            return View();
+            try
+            {
+                var city = _repoWrapper.City
+                    .FindByCondition(q => q.ID == cityId)
+                    .Include(c => c.CityAdministration)
+                    .ThenInclude(t => t.AdminType)
+                    .Include(n => n.CityAdministration)
+                    .ThenInclude(t => t.CityMembers)
+                    .ThenInclude(us => us.User)
+                    .Include(m => m.CityMembers)
+                    .ThenInclude(u => u.User)
+                    .FirstOrDefault();
+
+                var members = club.ClubMembers.Where(m => m.IsApproved).Take(6).ToList();
+                var followers = club.ClubMembers.Where(m => !m.IsApproved).Take(6).ToList();
+
+                var clubAdmin = club.ClubAdministration
+                    .Where(a => a.EndDate == null && a.AdminType.AdminTypeName == "Курінний")
+                    .Select(a => a.ClubMembers.User)
+                    .FirstOrDefault();
+                ViewBag.usermanager = _userManager;
+                return View(new ClubViewModel { Club = club, ClubAdmin = clubAdmin, Members = members, Followers = followers });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
         }
     }
 }
