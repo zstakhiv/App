4a5260656a0e7b1428e01998deb763e75e28e571
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2020-04-07 08:35:52 +0300

    add tests

--- EPlast/EPlast.XUnitTest/EventUserControllerTests.cs
+++ EPlast/EPlast.XUnitTest/EventUserControllerTests.cs
@@ -1,48 +1,68 @@
-﻿using EPlast.BussinessLayer.Interfaces;
+﻿using EPlast.BussinessLayer;
 using EPlast.Controllers;
 using EPlast.DataAccess.Entities;
 using EPlast.DataAccess.Repositories;
+using EPlast.Models.ViewModelInitializations.Interfaces;
 using EPlast.ViewModels;
+using EPlast.ViewModels.Events;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
+using Xunit;
 
 namespace EPlast.XUnitTest
 {
     public class EventUserControllerTests
     {
-        private EventUserController controller;
-        private ViewResult result;
-        private readonly IRepositoryWrapper _repoWrapper;
-        private readonly UserManager<User> _userManager;
-
-        [TestInitialize]
-        public void SetupContext()
+        public void EventCreateTest()
         {
-            controller = new EventUserController(_userManager,_repoWrapper);
-            result = controller.EventUser() as ViewResult;
-        }
+            // Arrange
+            var expected = new Event
+            {
+                EventName = "Event1",
+                EventType = new EventType { EventTypeName = "EventType1" },
+                EventCategory = new EventCategory { EventCategoryName = "EventCategory1" },
+                EventStatus = new EventStatus { EventStatusName = "EventStatus1" },
+                EventDateStart = DateTime.Now,
+                EventDateEnd = DateTime.Now,
+                Eventlocation = "Location1",
+                FormOfHolding = "FormOfHolding1",
+                ForWhom = "ForWhom1",
+                NumberOfPartisipants = 50,
+                Description = "Description1",
+                Questions = "Questions",
+            };
+            var repository = new Mock<IRepositoryWrapper>();
+            repository.Setup(r => r.Event.FindByCondition(It.IsAny<Expression<Func<Event, bool>>>())).Returns(new List<Event>{new Event
+            {
+                EventName = "Event1",
+                EventType = new EventType { EventTypeName = "EventType1" },
+                EventCategory = new EventCategory { EventCategoryName = "EventCategory1" },
+                EventStatus = new EventStatus { EventStatusName = "EventStatus1" },
+                EventDateStart = DateTime.Now,
+                EventDateEnd = DateTime.Now,
+                Eventlocation = "Location1",
+                FormOfHolding = "FormOfHolding1",
+                ForWhom = "ForWhom1",
+                NumberOfPartisipants = 50,
+                Description = "Description1",
+                Questions = "Questions",
+            } }.AsQueryable());
 
-        [TestMethod]
-        public void EventUserViewResultNotNull()
-        {
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void EventUserViewEqualEventUserCshtml()
-        {
-            Assert.AreEqual("EventUser", result.ViewName);
+            var userStoreMock = new Mock<IUserStore<User>>();
+            var usManager = new Mock<UserManager<User>>(userStoreMock.Object,
+               null, null, null, null, null, null, null, null);
+            var controller = new EventUserController(usManager.Object, repository.Object);
+            // Act
+            var events = new EventCreateViewModel { Event = expected };
+            var result = controller.EventCreate(events);
+            // Assert
+            repository.Verify(r => r.Event.Update(It.IsAny<Event>()), Times.Once());
         }
     }
 }
