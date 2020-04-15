3c5d1250f398d75165b02a675d3ea3cce6bada3d
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2020-04-15 22:25:00 +0300

    Fix date format

--- EPlast/EPlast/wwwroot/uncompiled/ts/eventCreate.js
+++ EPlast/EPlast/wwwroot/uncompiled/ts/eventCreate.js
@@ -2,5 +2,25 @@ $(document).ready(function () {
     $('#block button').click(function () {
         $('#name').val(this.id);
     });
+    $("#txtFromDate").datepicker({
+        dateFormat: "dd-mm-yy",
+        changeMonth: true,
+        changeYear: true,
+        yearRange: 'c-0:c+nn',
+        minDate: 'd',
+        onSelect: function (selected) {
+            $("#txtToDate").datepicker("option", "minDate", selected);
+        }
+    });
+    $("#txtToDate").datepicker({
+        dateFormat: "dd-mm-yy",
+        changeMonth: true,
+        changeYear: true,
+        yearRange: 'c-0:c+nn',
+        minDate: 'd',
+        onSelect: function (selected) {
+            $("#txtFromDate").datepicker("option", "maxDate", selected);
+        }
+    });
 });
 //# sourceMappingURL=eventCreate.js.map
