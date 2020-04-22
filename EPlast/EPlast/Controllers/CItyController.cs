448b15d12cfba4275cfbb68b4a98d746044d19c0
Ilona Hirnyk (Maksymovych) <cat.in.tardis@gmail.com>
2020-04-22 11:59:16 +0300

    Add edit page

--- EPlast/EPlast/Controllers/CItyController.cs
+++ EPlast/EPlast/Controllers/CItyController.cs
@@ -92,7 +92,7 @@ namespace EPlast.Controllers
 
                 var members = city.CityMembers.Where(m => m.EndDate == null && m.StartDate != null).ToList();
 
-                return View(new CityViewModel { Members = members });
+                return View(new CityViewModel { City = city, Members = members });
             }
             catch (Exception e)
             {
@@ -118,7 +118,7 @@ namespace EPlast.Controllers
 
                 var followers = city.CityMembers.Where(m => m.EndDate == null && m.StartDate == null).ToList();
 
-                return View(new CityViewModel { Followers = followers });
+                return View(new CityViewModel { City = city, Followers = followers });
             }
             catch (Exception e)
             {
@@ -146,7 +146,37 @@ namespace EPlast.Controllers
                                     .Where(a => a.EndDate == null && a.AdminType.AdminTypeName != "Голова Станиці")
                                     .ToList();
 
-                return View(new CityViewModel { CityAdmins = cityAdmins });
+                return View(new CityViewModel { City = city, CityAdmins = cityAdmins });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("HandleError", "Error", new { code = 505 });
+            }
+        }
+
+        public IActionResult Edit(int cityid)
+        {
+            try
+            {
+                var city = _repoWrapper.City
+                    .FindByCondition(q => q.ID == cityid)
+                    .Include(c => c.CityAdministration)
+                    .ThenInclude(t => t.AdminType)
+                    .Include(k => k.CityAdministration)
+                    .ThenInclude(a => a.User)
+                    .Include(m => m.CityMembers)
+                    .ThenInclude(u => u.User)
+                    .Include(l => l.CityDocuments)
+                    .ThenInclude(d => d.CityDocumentType)
+                    .FirstOrDefault();
+
+                var cityAdmins = city.CityAdministration
+                                    .Where(a => a.EndDate == null && a.AdminType.AdminTypeName != "Голова Станиці")
+                                    .ToList();
+                var members = city.CityMembers.Where(m => m.EndDate == null && m.StartDate != null).ToList();
+                var followers = city.CityMembers.Where(m => m.EndDate == null && m.StartDate == null).ToList();
+
+                return View(new CityViewModel { City = city, CityAdmins = cityAdmins, Members = members, Followers = followers });
             }
             catch (Exception e)
             {
