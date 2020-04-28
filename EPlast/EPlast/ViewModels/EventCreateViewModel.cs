493ce074ea5595bfe8601524b259831e9f58cd8b
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2020-04-28 13:17:27 +0300

    Save changes, add styles.

--- EPlast/EPlast/ViewModels/EventCreateViewModel.cs
+++ EPlast/EPlast/ViewModels/EventCreateViewModel.cs
@@ -17,6 +17,7 @@ namespace EPlast.ViewModels.Events
         public IEnumerable<SelectListItem> EventCategories { get; set; }
         public IEnumerable<EventType> EventTypes { get; set; }
         public IEnumerable<EventAdmin> EventAdmins { get; set; }
+        public EventStatus EventStatuses { get; set; }
         public IEnumerable<EventAdministration> EventAdministrations { get; set; }
         public IEnumerable<User> Users { get; set; }
         public User User { get; set; }
